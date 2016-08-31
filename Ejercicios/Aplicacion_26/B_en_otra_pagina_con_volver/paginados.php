d406e16e66e84c89e48946484cb6c8f083628994
maurosaquino <mauro.s.aquino@gmail.com>
2016-08-31 14:56:19 -0300

    Aplicacion 26 terminada en ambas opciones

---
+++ Ejercicios/Aplicacion_26/B_en_otra_pagina_con_volver/paginados.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+
+
+<?php
+
+ if (isset($_REQUEST['calculo'])){
+   
+    if ($_REQUEST['calculo'] == 'superficie'){
+        
+        echo '<div class="login-card">';
+        echo '<h1>Superficie</h1>';
+  
+         if (isset($_REQUEST['alto']) && isset($_REQUEST['ancho'])) {
+ 
+            echo $_REQUEST['alto'] * $_REQUEST['ancho'];
+ 
+        }
+ 
+        echo '</div>';
+     } else {
+    
+        echo '<div class="login-card">';
+        echo '<h1>Area</h1>';
+  
+        if (isset($_REQUEST['alto']) && isset($_REQUEST['ancho'])) {
+ 
+            echo (2*$_REQUEST['alto'] + 2*$_REQUEST['ancho']);
+        }
+
+        echo '</div>';
+
+        } 
+  }
+?>
+
+  <div class="login-card">
+	<form action="Index.html">
+	<br>
+	<input class="login-submit" type="submit" value="Volver">
+	</form>
+
+  </div>
+
+
+</body>
+</html>
+
